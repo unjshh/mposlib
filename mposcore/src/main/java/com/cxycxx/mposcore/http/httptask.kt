4dcdf955abc0cb582853107bab96c80bd6633637
pjl <unjshh@sohu.com>
2023-10-23 10:11:58 +0800

    修改launchHttpTask

--- mposcore/src/main/java/com/cxycxx/mposcore/http/httptask.kt
+++ mposcore/src/main/java/com/cxycxx/mposcore/http/httptask.kt
@@ -2,31 +2,57 @@ package com.cxycxx.mposcore.http
 
 import android.content.Context
 import com.cxycxx.mposcore.util.Util
+import com.cxycxx.mposcore.util.joAsString
 import com.google.gson.JsonObject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
+
+const val HTTP_TASK_MSG_NAME = "message"
+const val HTTP_TASK_RESULT_NAME = "result"
+const val HTTP_TASK_RESULT_OK = "成功"
+typealias HttpTaskRequest = JsonObject
+typealias HttpTaskResponse = JsonObject
+
+/**
+ * 方法名称
+ */
+var HttpTaskRequest.methodName: String
+    set(value) = addProperty("methodName", value)
+    get() = joAsString("methodName")
+
+/**
+ * 响应是否成功
+ */
+val HttpTaskResponse.isOKInRes get() = joAsString(HTTP_TASK_RESULT_NAME) == HTTP_TASK_RESULT_OK
+
+/**
+ * 回应信息
+ */
+val HttpTaskResponse.msgInRes get() = joAsString(HTTP_TASK_MSG_NAME)
 
 /**
  * 启动网络请求
  */
 @OptIn(ExperimentalCoroutinesApi::class)
 suspend fun launchHttpTask(
-    ps: JsonObject? = null,
+    ps: HttpTaskRequest,
     context: Context? = null,
-    dealErrorModel: Int = HttpTask.AUTO_SHOW_ERROR
-): JsonObject? =
+    dealErrorModel: Int = HttpTask.AUTO_SHOW_ERROR,
+    isNeedShowWaitDialog: Boolean = true
+): HttpTaskResponse? =
     suspendCancellableCoroutine { scc ->
         HttpTask(context) {
-            if (it["result"]?.asString == "成功" || dealErrorModel == HttpTask.ALWAYS_CALLBACK) {
+            if (it.isOKInRes || dealErrorModel == HttpTask.ALWAYS_CALLBACK) {
                 scc.resume(it)
                 return@HttpTask
             }
             if (dealErrorModel == HttpTask.AUTO_SHOW_ERROR) {
-                Util.showMsg(context, it["message"]?.asString ?: "")
+                Util.showMsg(context, it.msgInRes)
             }
             scc.resume(null)
         }.apply {
+            this.isNeedShowWaitDialog = isNeedShowWaitDialog
             setDealErrorModel(HttpTask.ALWAYS_CALLBACK)
         }.launch(ps)
     }
